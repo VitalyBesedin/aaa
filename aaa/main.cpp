aa2c623cfb8b1c8356359d65bed0ddb81fd54975
VitalyBesedin <vitaly_besedin@hotmail.com>
2019-01-26 18:55:03 +0300

    Initial commit

---
+++ aaa/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  aaa
+//
+//  Created by Vitaly Besedin on 26/01/2019.
+//  Copyright © 2019 Vitaly Besedin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int a;
+    std::cin >> a;
+    return 0;
+}
